882c78d999b3fd8b911223efdf481b57aa9786c4
HemantThakurCGC <152248321+HemantThakurCGC@users.noreply.github.com>
2023-11-28 18:00:07 +0530

    Update pow.java
    Optimzise solution

--- pow.java
+++ pow.java
@@ -1,13 +1,19 @@
 //Power of  a is b a=2,b=10 a power b-> 2 ^10=1024
 
-public class MyClass {
+public class MyClass {  
     public static int pow(int a,int b){
-        if(b==0)return 1;   
-        return a*pow(a,b-1);
-    }  
+        if(b==0)return 1;
+      //  return a*pow(a,b-1);
+      //Optimize Solution
+      int ans=pow(a,b/2);
+      if(b%2==0){
+          return ans * ans;
+      }
+      return a*ans*ans;
+    } 
     public static void main(String args[]) {
       int a=2;
-      int b=10;
+      int b=9;
         System.out.println(a+ " Power "+b +"  is :" + pow(a,b) );
     }
 }
